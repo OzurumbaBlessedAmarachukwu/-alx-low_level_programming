bcab678d098f572a7ee712e02727fb2d7e037d3d
OzurumbaBlessedAmarachukwu <amarachukwublessedozurumba@gmail.com>
2022-03-15 18:15:22 +0100

    second too

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - print lowercase
+* Description:Print lowercase alphabet
+* Return: 0
+*/
+void print_alphabet(void)
+{
+int alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+}
